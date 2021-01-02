bc8fd5e9c5b7e05fe740eefca81df903f655688d
trinhhuuhieu99 <trinhhuuhieu99@gmail.com>
2021-01-02 11:31:33 +0700

    hieu.th add choose type

--- aes.class.php
+++ aes.class.php
@@ -3,40 +3,57 @@
 
 class aes
 {
-	private $secret_iv = 'This is my secret iv';
-
 	/**
 	 * Decrypted string
 	 * @param string $data Character string
-	 * @return string
+	 * @return array
 	 */
 	public function decode($str, $key, $method)
 	{
 		$starttime = microtime(true);
-		$testTime = openssl_decrypt(base64_decode($str), $method, hash('sha256', $key), OPENSSL_RAW_DATA, $this->getIv());
+		$type = $this->__chooseType($key, $method);
+		$result = openssl_decrypt(base64_decode($str), $type[1], $type[0], OPENSSL_RAW_DATA, '123456789abcdefg');
 		$endtime = microtime(true);
 		$timediff = round((($endtime - $starttime) * 1000), 5);
-		return [$timediff, $testTime];
+		return [$timediff, $result];
 	}
 
 	/**
 	 * Encrypted string
 	 * @param string $data Character string
-	 * @return string
+	 * @return array
 	 */
 	public function encode($str, $key, $method)
 	{
 		$starttime = microtime(true);
-		$testTime = base64_encode(openssl_encrypt($str, $method, hash('sha256', $key), OPENSSL_RAW_DATA,
-			$this->getIv()));
+		$type = $this->__chooseType($key, $method);
+		$result = base64_encode(openssl_encrypt($str, $type[1], $type[0], OPENSSL_RAW_DATA, '123456789abcdefg'));
 		$endtime = microtime(true);
 		$timediff = round((($endtime - $starttime) * 1000), 5);
-		return [$timediff, $testTime];
+		return [$timediff, $result];
 	}
 
-	private function getIv()
+	private function __chooseType($key, $method)
 	{
-		return substr(hash('sha256', $this->secret_iv), 0, 16);
-	}
+		$hashKey = "";
+		$defineMethod = "";
+		if (!empty($key) && !empty($method)) {
+			switch ($method) {
+				case "128":
+				$hashKey = substr(hash('sha256', $key), 0, 16);
+				$defineMethod = 'AES-128-CBC';
+				break;
+				case "192":
+				$hashKey = substr(hash('sha256', $key), 0, 24);
+				$defineMethod = 'AES-192-CBC';
+				break;
+				case "256":
+				$hashKey = substr(hash('sha256', $key), 0, 32);
+				$defineMethod = 'AES-256-CBC';
+				break;
+			}
+		}
 
+		return [$hashKey, $defineMethod];
+	}
 }
